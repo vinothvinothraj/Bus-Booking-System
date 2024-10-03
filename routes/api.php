7054a79f8ddbae68e2d687f8eed9f0ebe26c24af
Vinoth07 <sangaralingamvinoth64@gmail.com>
2024-10-03 18:22:07 +0530

    service-api-added

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,14 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\ServicesController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+// Route to get all services
+Route::get('/services', [ServicesController::class, 'index']);
+
+// Route to get a specific service by ID
+Route::get('/services/{id}', [ServicesController::class, 'show']);
