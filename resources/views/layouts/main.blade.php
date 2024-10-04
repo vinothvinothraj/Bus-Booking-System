b637da9cff453b7d0204b956bba65e39d097ae3c
Vinoth07 <sangaralingamvinoth64@gmail.com>
2024-10-04 15:02:43 +0530

    angular-setup-fetch-services

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,9 +1,14 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" ng-app="servicesApp">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $title ?? 'Bus Tickets Booking System' }}</title>
+
+    <!-- AngularJS -->
+    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.8.2/angular.min.js"></script>
+
+    <!-- Tailwind CSS (Vite) -->
     @vite('resources/css/app.css')
     @livewireStyles
 </head>
