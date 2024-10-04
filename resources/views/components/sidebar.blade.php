a25a3f872d50c30b264b1475e0ba88c9d5c486d6
Vinoth07 <sangaralingamvinoth64@gmail.com>
2024-10-04 17:45:03 +0530

    dark-theme-conf-added

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,38 +1,116 @@
 <div class="h-full">
     <nav class="mt-4 px-6">
-        <a href="{{ route('dashboard') }}" class="nav-item text-gray-600 dark:text-gray-400 block px-4 py-2 text-lg font-medium">
+        
+        <a href="{{ route('dashboard') }}" class="nav-item text-gray-600 dark:text-gray-400 block px-4 py-2 text-md font-medium justify-center">
+            
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 inline-block mr-2" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M12 21v-8.25M15.75 21v-8.25M8.25 21v-8.25M3 9l9-6 9 6m-1.5 12V10.332A48.36 48.36 0 0 0 12 9.75c-2.551 0-5.056.2-7.5.582V21M3 21h18M12 6.75h.008v.008H12V6.75Z" />
+            </svg>
+              
             Dashboard
         </a>
-        <a href="{{ route('profile.show') }}" class="nav-item text-gray-600 dark:text-gray-400 block px-4 py-2 text-lg font-medium">
-            Profile
-        </a>
-        <a href="{{ route('users') }}" class="nav-item text-gray-600 dark:text-gray-400 block px-4 py-2 text-lg font-medium">
-            Users
-        </a>
-        <a href="{{ route('services') }}" class="nav-item text-gray-600 dark:text-gray-400 block px-4 py-2 text-lg font-medium">
-            Services
-        </a>
-        <a href="{{ route('bus-details') }}" class="nav-item text-gray-600 dark:text-gray-400 block px-4 py-2 text-lg font-medium">
-            Bus Details
-        </a>
-        <a href="{{ route('route-details') }}" class="nav-item text-gray-600 dark:text-gray-400 block px-4 py-2 text-lg font-medium">
-            Routes Details
-        </a>
-        <a href="{{ route('time-slots-details') }}" class="nav-item text-gray-600 dark:text-gray-400 block px-4 py-2 text-lg font-medium">
-            TimeSlot Details
-        </a>
+        
+        
 
-        <a href="{{ route('booking-details') }}" class="nav-item text-gray-600 dark:text-gray-400 block px-4 py-2 text-lg font-medium">
+        <a href="{{ route('booking-details') }}" class="nav-item text-gray-600 dark:text-gray-400 block px-4 py-2 text-md font-medium">
+                    
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 inline-block mr-2" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 3.75V16.5L12 14.25 7.5 16.5V3.75m9 0H18A2.25 2.25 0 0 1 20.25 6v12A2.25 2.25 0 0 1 18 20.25H6A2.25 2.25 0 0 1 3.75 18V6A2.25 2.25 0 0 1 6 3.75h1.5m9 0h-9" />
+            </svg>
+              
             Booking Details
         </a>
-
-        <a href="{{ route('payment-details') }}" class="nav-item text-gray-600 dark:text-gray-400 block px-4 py-2 text-lg font-medium">
+        <a href="{{ route('payment-details') }}" class="nav-item text-gray-600 dark:text-gray-400 block px-4 py-2 text-md font-medium">
+            
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 inline-block mr-2" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 8.25h19.5M2.25 9h19.5m-16.5 5.25h6m-6 2.25h3m-3.75 3h15a2.25 2.25 0 0 0 2.25-2.25V6.75A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25v10.5A2.25 2.25 0 0 0 4.5 19.5Z" />
+            </svg>
+              
             Payment Details
         </a>
-
-        <a href="{{ route('point-system-details') }}" class="nav-item text-gray-600 dark:text-gray-400 block px-4 py-2 text-lg font-medium">
+        <a href="{{ route('point-system-details') }}" class="nav-item text-gray-600 dark:text-gray-400 block px-4 py-2 text-md font-medium">
+            
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 inline-block mr-2" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M21 11.25v8.25a1.5 1.5 0 0 1-1.5 1.5H5.25a1.5 1.5 0 0 1-1.5-1.5v-8.25M12 4.875A2.625 2.625 0 1 0 9.375 7.5H12m0-2.625V7.5m0-2.625A2.625 2.625 0 1 1 14.625 7.5H12m0 0V21m-8.625-9.75h18c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125h-18c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z" />
+            </svg>
+              
             Points System
         </a>
 
+        
+        <div x-data="{ open: false }">
+            <button @click="open = !open" class="nav-item text-gray-600 dark:text-gray-400 block px-4 py-2 text-md font-medium flex items-center">
+                
+                <svg :class="{'rotate-180': open}" class="h-5 w-5 transform transition-transform inline-block mr-3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
+                </svg>
+                More Options
+            </button>
+
+            
+            <div x-show="open" class="ml-6">
+                <a href="{{ route('services') }}" class="nav-item text-gray-600 dark:text-gray-400 block px-4 py-2 text-md font-medium">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 inline-block mr-2" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75m-3-7.036A11.959 11.959 0 0 1 3.598 6 11.99 11.99 0 0 0 3 9.749c0 5.592 3.824 10.29 9 11.623 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.571-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.285Z" />
+                    </svg>
+                      
+                    Services
+                </a>
+                <a href="{{ route('bus-details') }}" class="nav-item text-gray-600 dark:text-gray-400 block px-4 py-2 text-md font-medium">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 inline-block mr-2" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M9 3.75H6.912a2.25 2.25 0 0 0-2.15 1.588L2.35 13.177a2.25 2.25 0 0 0-.1.661V18a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18v-4.162c0-.224-.034-.447-.1-.661L19.24 5.338a2.25 2.25 0 0 0-2.15-1.588H15M2.25 13.5h3.86a2.25 2.25 0 0 1 2.012 1.244l.256.512a2.25 2.25 0 0 0 2.013 1.244h3.218a2.25 2.25 0 0 0 2.013-1.244l.256-.512a2.25 2.25 0 0 1 2.013-1.244h3.859M12 3v8.25m0 0-3-3m3 3 3-3" />
+                    </svg>
+                      
+                    Bus Details
+                </a>
+                <a href="{{ route('route-details') }}" class="nav-item text-gray-600 dark:text-gray-400 block px-4 py-2 text-md font-medium">
+                    <!-- Heroicon for routes -->
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 inline-block mr-2" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M13.19 8.688a4.5 4.5 0 0 1 1.242 7.244l-4.5 4.5a4.5 4.5 0 0 1-6.364-6.364l1.757-1.757m13.35-.622 1.757-1.757a4.5 4.5 0 0 0-6.364-6.364l-4.5 4.5a4.5 4.5 0 0 0 1.242 7.244" />
+                    </svg>
+                      
+                    Routes Details
+                </a>
+                <a href="{{ route('time-slots-details') }}" class="nav-item text-gray-600 dark:text-gray-400 block px-4 py-2 text-md font-medium">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 inline-block mr-2" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                    </svg>
+                      
+                    TimeSlot Details
+                </a>
+                
+            </div>
+        </div>
+
+        <a href="{{ route('profile.show') }}" class="nav-item text-gray-600 dark:text-gray-400 block px-4 py-2 text-md font-medium">
+            
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 inline-block mr-2" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+            </svg>
+              
+            Profile
+        </a>
+        
+        <a href="{{ route('users') }}" class="nav-item text-gray-600 dark:text-gray-400 block px-4 py-2 text-md font-medium">
+            
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 inline-block mr-2" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M18 18.72a9.094 9.094 0 0 0 3.741-.479 3 3 0 0 0-4.682-2.72m.94 3.198.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0 1 12 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 0 1 6 18.719m12 0a5.971 5.971 0 0 0-.941-3.197m0 0A5.995 5.995 0 0 0 12 12.75a5.995 5.995 0 0 0-5.058 2.772m0 0a3 3 0 0 0-4.681 2.72 8.986 8.986 0 0 0 3.74.477m.94-3.197a5.971 5.971 0 0 0-.94 3.197M15 6.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm6 3a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Zm-13.5 0a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Z" />
+            </svg>
+              
+            Users
+        </a>
+
+        <a href="{{ route('logout') }}" class="nav-item text-gray-600 dark:text-gray-400 block px-4 py-2 text-md font-medium"
+        onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 inline-block mr-2" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15m3 0 3-3m0 0-3-3m3 3H9" />
+            </svg>
+            Logout
+        </a>
+
+        <form id="logout-form" method="POST" action="{{ route('logout') }}" class="hidden">
+            @csrf
+        </form>
     </nav>
 </div>
--- resources/views/livewire/top-navigation-bar.blade.php
+++ resources/views/livewire/top-navigation-bar.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <header class="bg-white">
+    <header class="bg-white dark:bg-gray-800">
         <div class="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
           <div class="flex h-16 items-center justify-between">
             <div class="md:flex md:items-center md:gap-12">
@@ -46,6 +46,12 @@
       
             <div class="flex items-center gap-4">
               <div class="sm:flex sm:gap-4">
+
+                <button id="theme-toggle" type="button" class="p-2 mr-1 text-blue-500 rounded-lg  dark:text-gray-400 ">
+                  <svg id="theme-toggle-dark-icon" class="hidden w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path></svg>
+                  <svg id="theme-toggle-light-icon" class="hidden w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z" fill-rule="evenodd" clip-rule="evenodd"></path></svg>
+                </button>
+
                 @if (Route::has('login'))
                   @auth
                     <a
@@ -92,4 +98,37 @@
           </div>
         </div>
     </header>
+
+    <script>
+      const themeToggle = document.getElementById('theme-toggle');
+      const darkIcon = document.getElementById('theme-toggle-dark-icon');
+      const lightIcon = document.getElementById('theme-toggle-light-icon');
+  
+      // Get current theme from localStorage
+      const currentTheme = localStorage.getItem('theme');
+  
+      if (currentTheme === 'dark') {
+          document.documentElement.classList.add('dark');
+          darkIcon.classList.remove('hidden');
+          lightIcon.classList.add('hidden');
+      } else {
+          document.documentElement.classList.remove('dark');
+          lightIcon.classList.remove('hidden');
+          darkIcon.classList.add('hidden');
+      }
+  
+      // Toggle theme when button is clicked
+      themeToggle.addEventListener('click', function() {
+          darkIcon.classList.toggle('hidden');
+          lightIcon.classList.toggle('hidden');
+  
+          if (document.documentElement.classList.contains('dark')) {
+              document.documentElement.classList.remove('dark');
+              localStorage.setItem('theme', 'light');
+          } else {
+              document.documentElement.classList.add('dark');
+              localStorage.setItem('theme', 'dark');
+          }
+      });
+  </script>
 </div>
